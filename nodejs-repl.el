78f98c5dace317030a15f839d39fc15934e5bba8
abicky <takeshi.arabiki@gmail.com>
2018-09-18 02:00:29 +0900

    Improve README

--- nodejs-repl.el
+++ nodejs-repl.el
@@ -44,6 +44,26 @@
 ;;                 (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
 ;;                 (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
 ;;
+;; When a version manager such as nvm is used to run different versions
+;; of Node.js, it is often desirable to start the REPL of the version
+;; specified in the .nvmrc file per project.  In such case, customize the
+;; `nodejs-repl-command` variable with a function symbol.  That function
+;; should query nvm for the Node.js command to run.  For example:
+;;
+;;     (require 'nodejs-repl)
+;;     (defun nvm-which ()
+;;       (let* ((shell (concat (getenv "SHELL") " -l -c 'nvm which'"))
+;;              (output (shell-command-to-string shell)))
+;;         (cadr (split-string output "[\n]+" t))))
+;;     (setq nodejs-repl-command #'nvm-which)
+;;
+;; The `nvm-which` function can be simpler, and perhaps can run faster,
+;; too, if using Bash:
+;;
+;;     (defun nvm-which ()
+;;       (let ((output (shell-command-to-string "source ~/.nvm/nvm.sh; nvm which")))
+;;         (cadr (split-string output "[\n]+" t))))
+;;
 
 (require 'cc-mode)
 (require 'comint)
