93ebc0e4cd00ab3fbd4705e27004323d923d5001
abicky <takeshi.arabiki@gmail.com>
2020-01-14 15:47:39 +0900

    Support Node.js 13
    A preview feature was introduced by
    https://github.com/BridgeAR/node/commit/6bdf8d106009e4ed0f3c323d0f0874a51acc8e08
    in this version and nodejs-repl doesn't work
    if the feature is enabled.
    cf. https://github.com/abicky/nodejs-repl.el/issues/28

--- nodejs-repl.el
+++ nodejs-repl.el
@@ -140,7 +140,7 @@ See also `comint-process-echoes'"
 (defvar nodejs-repl-code-format
   (concat
    "require('repl').start({prompt: '%s', useGlobal: %s, replMode: "
-   "require('repl')['REPL_MODE_' + '%s'.toUpperCase()] })"))
+   "require('repl')['REPL_MODE_' + '%s'.toUpperCase()], preview: false})"))
 
 (defvar nodejs-repl-extra-espace-sequence-re "\\(\x1b\\[[0-9]+[GJK]\\)")
 
