1138b40931db86b2b6d80e6d9891a6cd1fd71189
abicky <takeshi.arabiki@gmail.com>
2020-02-23 23:45:23 +0900

    Bump the version number to 0.2.4

--- nodejs-repl.el
+++ nodejs-repl.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012-2020  Takeshi Arabiki
 
 ;; Author: Takeshi Arabiki
-;; Version: 0.2.3
+;; Version: 0.2.4
 
 ;;  This program is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License as published by
@@ -73,7 +73,7 @@
   "Run Node.js REPL and communicate the process."
   :group 'processes)
 
-(defconst nodejs-repl-version "0.2.3"
+(defconst nodejs-repl-version "0.2.4"
   "Node.js mode Version.")
 
 (defcustom nodejs-repl-command "node"
