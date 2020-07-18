4693d597fc0b306bea8c989c0364094c6761ff91
wailo <wailo_2@hotmail.com>
2020-07-19 01:48:16 +0200

    Added key binding for send-buffer command
    Same as python shell in emacs, C-c C-c is used to send buffer to repl.

--- nodejs-repl.el
+++ nodejs-repl.el
@@ -43,6 +43,7 @@
 ;;                 (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
 ;;                 (define-key js-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
 ;;                 (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
+;;                 (define-key js-mode-map (kbd "C-c C-c") 'nodejs-repl-send-buffer)
 ;;                 (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
 ;;                 (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
 ;;
